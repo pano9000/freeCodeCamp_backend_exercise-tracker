879a4960d3d98e959704e3ce8a481128ca147438
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-01-12 09:34:44 +0100

    routes: add validation to logsGet and exercisePost

--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -1,17 +1,28 @@
 const router = require("express").Router();
 const controllers = require("../controllers")
+const validation = require("../services/validationSchemas/")
 
 
 router.get("*", controllers.rateLimiter.get);
 router.post("*", controllers.rateLimiter.post);
 
-router.get("/", controllers.root.get)
+router.get("/", controllers.root.get);
 
-router.get("/api/users", controllers.api.users.get)
-router.get("/api/users/:userId/logs", controllers.api.users.logs.get)
+router.get("/api/users", controllers.api.users.get);
+router.get(
+  "/api/users/:userId/logs",
+  validation.check.logsGet,
+  validation.handler,
+  controllers.api.users.logs.get);
 
-router.post("/api/users", controllers.api.users.post )
-router.post("/api/users/:userId/exercises", controllers.api.users.exercises.post)
+router.post("/api/users", controllers.api.users.post);
+
+router.post(
+  "/api/users/:userId/exercises",
+  validation.check.exercisePost,
+  validation.handler,
+  controllers.api.users.exercises.post
+);
 
 
 /* future routes
