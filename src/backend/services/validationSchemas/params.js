01f0a0e8118818f7c0c6b3021952f3a8105fb2f3
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-01-12 09:10:25 +0100

    validation: reformat params for easier reading

--- src/backend/services/validationSchemas/params.js
+++ src/backend/services/validationSchemas/params.js
@@ -1,37 +1,36 @@
 const { param, query } = require("express-validator")
 
-/*
-function createQueryParams(paramNames) {
-
-  paramNames.forEach(param => {
-
-    this[param[0]] = param[1]
-
-  })
-
-}
-
-new createQueryParams([
-
-  "from": query("from").optional( { checkFalsy: true } ).isDate(),
-  "to": query("to").optional().isDate(),
-  "limit": query("limit").optional().isInt(),
-  "sort": query("sort").optional().custom(value => ["asc", "desc"].includes(value)),
-
-
-])
-*/
-
 const queryParams = {
-  "from": query("from").optional( { checkFalsy: true } ).isDate(),
-  "to": query("to").optional().isDate(),
-  "limit": query("limit").optional().isInt(),
-  "sort": query("sort").optional().custom(value => ["asc", "desc"].includes(value)),
+
+  "from":
+    query("from")
+    .optional( { checkFalsy: true } )
+    .isDate(),
+
+  "to":
+    query("to")
+    .optional()
+    .isDate(),
+
+  "limit":
+    query("limit")
+    .optional()
+    .isInt(),
+
+  "sort":
+    query("sort")
+    .optional()
+    .isIn(["asc", "desc"]),
 }
 
 const pathParams = {
-  "userId": param("userId"), // add regex here for userId,
-  "exerciseId": param("exerciseId")
+  "userId":
+    param("userId")
+    .matches(/^user_[a-z0-9]{24}$/),
+
+  "exerciseId":
+    param("exerciseId")
+    .matches(/^exerc_[a-z0-9]{24}$/)
 }
 
 
