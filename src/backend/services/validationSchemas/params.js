49c8c835999ef5dd16f1af37cb0cba280edde904
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-01-13 01:02:45 +0100

    validation: add more advanced validation handler
    -> allows to reset "optional" query params to undefined, if they are invalid, instead of "throwing away" the request

--- src/backend/services/validationSchemas/params.js
+++ src/backend/services/validationSchemas/params.js
@@ -3,33 +3,33 @@ const { param, query } = require("express-validator")
 const queryParams = {
 
   "from":
-    query("from")
-    .optional( { checkFalsy: true } )
+    query("from", { errorHandling: "resetToDefault" })
+    .optional()
     .isDate(),
 
   "to":
-    query("to")
+    query("to", { errorHandling: "resetToDefault" })
     .optional()
     .isDate(),
 
   "limit":
-    query("limit")
+    query("limit", { errorHandling: "resetToDefault" })
     .optional()
     .isInt(),
 
   "sort":
-    query("sort")
+    query("sort", { errorHandling: "resetToDefault" })
     .optional()
     .isIn(["asc", "desc"]),
 }
 
 const pathParams = {
   "userId":
-    param("userId")
+    param("userId", { errorHandling: "sendError" })
     .matches(/^user_[a-z0-9]{24}$/),
 
   "exerciseId":
-    param("exerciseId")
+    param("exerciseId", { errorHandling: "sendError" })
     .matches(/^exerc_[a-z0-9]{24}$/)
 }
 
--- src/backend/services/validationSchemas/validation.handler.js
+++ src/backend/services/validationSchemas/validation.handler.js
@@ -3,17 +3,40 @@ const { validationResult } = require("express-validator")
 
 function handler(req, res, next) {
   const validationErrors = validationResult(req);
+  console.log("*************", validationErrors)
 
   if (validationErrors.isEmpty()) {
     next();
     return
   }
-  
-  console.log("*************", validationErrors)
-  return res.status(400).json({
-    error: false,
-    errors: validationErrors.array(),
-  });
+
+
+  //TODO: create new ErrorType -> ValidationError? and handle in ErrorMiddleware?
+  const fatalErrors = validationErrors.array().filter(error => error.msg.errorHandling === "sendError")
+
+  if ( fatalErrors.length > 0 ) {
+
+    const response = fatalErrors.map(fatalError => {
+      return {
+        msg: `Invalid value for field '${fatalError.param}'`,
+        value: fatalError.value
+      }
+    })
+
+    return res.status(400).json({error: response});
+  }
+
+  // if no "fatal" errors -> try to 
+  if ( validationErrors.errors.some( error => error.msg.errorHandling === "sendError") === false ) { //todo refactor
+    validationErrors.errors.forEach( elem => {
+      req[elem.location][elem.param] = undefined
+    })
+    next()
+    return
+
+  }
+
+
 
 
 }
